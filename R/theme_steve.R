721be659ada166b9ac6f0bdcfd62d7b12a48a7ff
svmiller <steven.v.miller@gmail.com>
2023-04-27 10:11:36 +0200

    dsafdsfsda

--- R/theme_steve.R
+++ R/theme_steve.R
@@ -1,4 +1,4 @@
-#' Steve's Preferred \pkg{ggplot2} Themes and Assorted Stuff
+#' Steve's Preferred \pkg{ggplot2} Theme
 #'
 #' @description \code{theme_steve()} is my default theme framework for graphs I
 #' make with \pkg{ggplot2}. It starts \code{theme_bw()}, which is available in
@@ -6,17 +6,34 @@
 #' result is, I think, a lovely template for graphs I make in R.
 #'
 #' @param style various styles/adjustments to make to the base theme. Must be
-#' one of the following: "web" (default), "ms", "fira", "custom", or "generic".
-#' The "custom" style is a fancy way of saying "supply your own fonts". Where
-#' "custom" is used in this argument, something must be supplied to the font
-#' argument in this same function.
+#' one of the following: "web" (default), "ms", "fira", "merri", "custom", or
+#' "generic". The "custom" style is a fancy way of saying "supply your own
+#' fonts". Where "custom" is used in this argument, something must be supplied
+#' to the font argument in this same function.
 #' @param font a character vector corresponding with a font that the user ideally
 #' has installed on their operating system.
 #' @param ... optional, called for side effects
 #'
-#' @details The best use of this function may involve the fonts you have installed
-#' on your system. The user should experiment with various options to see what they
-#' like. The "generic" style will use default \pkg{ggplot2} fonts.
+#' @details
+#'
+#' The best use of this function may involve the fonts you have installed on
+#' your system. The user should experiment with various options to see what they
+#' like. The "generic" style will use default \pkg{ggplot2} fonts and is invoked
+#' here in this simple function for CRAN testing requirements. The default is
+#' "web", which depends on having Open Sans and Titillium Web on the operating
+#' system. You will see these two fonts everywhere on my website and most of my
+#' R package websites.
+#'
+#' For greater clarity, the user should understand "ms" is code for the Crimson
+#' Pro font, which resembles the "cochineal" font family in the LaTeX font
+#' catalogue. "cochineal" is a fork of the Crimson font and its use is heavily
+#' implied by my preferred article template (`article2`) in \pkg{stevetemplates}.
+#'
+#' "fira" is shorthand for "Fira Sans". "merri" is shorthand for "Merriweather".
+#' The former features prominently in Firefox as well as my current operating
+#' system (Pop! OS). The latter incorporates the Merriweather font, which is a
+#' close cousin of the Palatino font. Palatino is the implied default font of
+#' my first article template (`article`) in \pkg{stevetemplates}.
 #'
 #' @return No return value. Function is used for its side effect, which is to
 #' format a plot made in the \pkg{ggplot2} package.
@@ -29,7 +46,7 @@
 #'
 theme_steve <- function(style="web", font, ...) {
 
-  if(!(style %in% c("web", "fira", "ms", "generic", "custom"))) {
+  if(!(style %in% c("web", "fira", "ms", "generic", "merri", "custom"))) {
     stop("This is not a recognized style in this function.")
   }
 
@@ -80,6 +97,17 @@ theme_steve <- function(style="web", font, ...) {
             text = element_text(family = "Fira Sans")) -> p
 
   } else if(style == "generic") {
+
+  } else if(style == "merri") {
+
+    p +
+      theme(plot.title = element_text(family = "Merriweather"),
+            legend.title = element_text(family = "Merriweather"),
+            plot.subtitle = element_text(family = "Merriweather"),
+            axis.title.y = element_text(family = "Merriweather"),
+            axis.title.x = element_text(family = "Merriweather"),
+            text = element_text(family = "Merriweather")) -> p
+
 
   } else if(style == "custom") {
 
